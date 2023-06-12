3b0206df921ae808ad2aab74f294bbfde4877e9b
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-06-12 21:31:04 +0300

    fixed bugs

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,8 +40,6 @@ const ContactForm = () => {
           onChange={e => setName(e.target.value)}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
       </label>
@@ -53,8 +51,6 @@ const ContactForm = () => {
           onChange={e => setNumber(e.target.value)}
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
       </label>
